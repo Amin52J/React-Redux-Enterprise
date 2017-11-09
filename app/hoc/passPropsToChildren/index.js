1c1824d992ca2adefef003eb10b33f1d882bff21
Amin Jafari <a.jafari.90@gmail.com>
2017-11-09 22:52:36 +0330

    Fixed eslint error

--- app/hoc/passPropsToChildren/index.js
+++ app/hoc/passPropsToChildren/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function (ComponentsSet, props) {
-  return React.Children.map(ComponentsSet, (child) => React.cloneElement(child, props))
+  return React.Children.map(ComponentsSet, child => React.cloneElement(child, props));
 }
